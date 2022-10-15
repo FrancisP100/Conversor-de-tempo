1c33f533cbb233758142bcfcfbbd6c212fb5eb4a
FrancisP100 <72936526+FrancisP100@users.noreply.github.com>
2022-10-15 07:53:28 +0100

    Conversor_de_horasV0.1
    Primeiro commit e em linguagem C

---
+++ Conversor de horas.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+main()
+{
+     long int x;
+    float res;
+    char unity;
+ 
+    printf("Escolha um numero de horas: \n");
+    scanf("%d",&x);
+    printf("Deseja ver em (D/M/A): \n");
+    scanf(" %c",&unity);
+ switch (unity)
+    {
+    case 'd' :
+    case 'D' :
+        res =  x / 24;
+        break;
+    case 'm' :
+    case 'M' :
+        res = x / 720;
+        break;
+    case 'a' :
+    case 'A' :
+        res = x / (720 * 12) ;
+        break;
+
+    default:
+           printf("Unidade   invalida\n"); 
+
+
+        break;
+    }
+    printf("%dh equivale a %.0f%c", x, res, unity);
+}
