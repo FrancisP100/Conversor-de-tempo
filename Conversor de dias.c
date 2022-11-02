db6888435cd2a476e9df6db430f2c3306e50038d
FrancisP100 <72936526+FrancisP100@users.noreply.github.com>
2022-11-02 11:15:19 +0100

    Just a few things

--- Conversor de dias.c
+++ Conversor de dias.c
@@ -4,7 +4,7 @@ main()
      long int x;
     float res;
     char unity;
- 
+ /* Estor a aprender novas coisas vais curtir bué*/
     printf("Escolha um número de dias: \n");
     scanf("%d",&x);
     printf("Deseja ver em (H/M/A): \n");
